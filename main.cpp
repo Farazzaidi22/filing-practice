fcc111e39febe0ff8184a2e287ae0a4dd3799e36
faraz zaidi <farazkhalid05@yahoo.com>
2018-04-26 23:36:24 +0500

    second commit

--- main.cpp
+++ main.cpp
@@ -8,8 +8,18 @@ int main()
   ofstream myfile;
   myfile.open("faraz.txt");
 
+  if(myfile.is_open())
+  {
+    cout<<"File object is associated with a file"<< endl;
+  }
+
+  else
+  {
+    cout<<"File object is not associated with a file" << endl;
+  }
+
   myfile<<"i love dc comics";
   myfile.close();
-
+  
   system("pause");
 }
